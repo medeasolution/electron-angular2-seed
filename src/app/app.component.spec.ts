95ed271819a3cb57bab66f5e7d7be903f6ce787f
psykologist90 <ericcobossanchez@gmail.com>
2016-11-23 09:13:08 +0100

    Trying to fix tests, still not working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 describe('App', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    TestBed.configureTestingModule({ declarations: [AppComponent], schemas: [ NO_ERRORS_SCHEMA ]});
   });
   it ('should work', () => {
     let fixture = TestBed.createComponent(AppComponent);
